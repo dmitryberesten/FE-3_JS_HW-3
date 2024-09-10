5b9759ef4d56bcbf1bdcd402de7f1509a1b3a1b1
Dima <dmitryberesten@gmail.com>
2024-09-11 01:08:34 +0300

    FE-3_JS_HW-3

---
+++ task-1/task-1.js
@@ -0,0 +1,7 @@
+// 1. Вивести result в консоль. Перевірити тип змінної result= 5+5+’5’.
+
+let result = 5 + 5 + "5";
+
+console.log(result);
+
+console.log(typeof result);
---
+++ task-3/task-3.js
@@ -0,0 +1,18 @@
+// 1. Записати в окремих змінних кожне слово: My, name, is.
+// 2. Поєднати всі слова в змінну fullName.
+// 3. До змінної fullName додати ‘Viktor’.
+// 4. fullName вивести в консоль.
+
+// 1. Створюємо окремі змінні для кожного слова
+let word1 = "My";
+let word2 = "name";
+let word3 = "is";
+
+// 2. Поєднуємо всі слова в змінну fullName
+let fullName = word1 + " " + word2 + " " + word3;
+
+// 3. Додаємо до змінної fullName слово 'Viktor'
+fullName += " Viktor";
+
+// 4. Виводимо значення змінної fullName в консоль
+console.log(fullName);
